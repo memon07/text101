d225633b62535f23999d976e2b0a685a5d305c6a
memon07 <memonshoyeb26@gmail.com>
2019-06-09 23:06:04 +0530

    state object

--- New Unity Project/Assets/AdventureGame.cs
+++ New Unity Project/Assets/AdventureGame.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] state startingState;
+
+    state state;
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = ("this is the text ");
+        state = startingState;
+        textComponent.text = state.GetStateStory();
     }
 
     // Update is called once per frame
---
+++ New Unity Project/Assets/state.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "state")]
+public class state : ScriptableObject
+{
+   [TextArea(14,10)] [SerializeField] string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+}
